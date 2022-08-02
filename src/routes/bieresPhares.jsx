6cc522f6b88c90fbec182b5192b5fa2620c7a0b2
Olivia Peny-Coblentz <oliv@octo.com>
2022-08-02 10:24:18 +0200

    update version

--- src/routes/bieresPhares.jsx
+++ src/routes/bieresPhares.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import  React, { useEffect, useState } from "react";
 import axios from 'axios';
+
 function BieresPhares() {
   const [beerList, setBeerList] = useState([])
   useEffect(() => {
